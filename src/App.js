946fc1cf4004ababf84ae0f411cb302874df629f
lenayj <lena@uniortholab.com>
2021-03-02 11:45:34 -0800

    google/MS login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React,{Component} from 'react';
 import Routers from './components/Router'; 
+
+
 class App extends Component {
   render() {
       return (
         <>
         <Routers/>
         </>
+
       );
   }
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,40 +2,60 @@ import '../css/Login.css';
 
 import {Button, Form, FormGroup, Input} from 'reactstrap';
 import GoogleLogin from 'react-google-login';
-import {React} from "react";
+import {React, Component} from "react";
 import { Link } from 'react-router-dom';
 import MicrosoftLogin from "react-microsoft-login";
 
 
-const Login = () => {
-
-  return (
-    <Form className="login-form">
-      <h2 className="text-center">Welcome to Universal RX</h2>
-      <h6 className="text-center welcomedes">Universal Orthodontic Lab Digital Prescription</h6>
-      <FormGroup>
-        <Input className="acctlogin" type="email" placeholder="Username"></Input>
-      </FormGroup>
-      <FormGroup>
-        <Input className="acctlogin" type="password" placeholder="Password"></Input>
-      </FormGroup>
-      <div className="text-left">
-        <a href="/forgot-password"><span className="pt-3">Forgot Your Password?</span></a>
-      </div>
-      <Button className="btn-lg btn-primary btn-block">Log in</Button>
-      <div className="text-center pt-3">Or continue with your social account</div>
-      <GoogleLogin 
-        className="mt-3 mb-3"
-        buttonText="Login with Google"
-        theme= "dark" />
-      <MicrosoftLogin />
-      <div className="text-left pt-3">Do you have an account? 
-        
-        <Link to= '/SignUp' className="links"> Join free today</Link>
-      </div>
-    </Form>
-    
-  );
+class Login extends Component {  
+  responseGoogle=(response)=>{
+    console.log(response);
+    console.log(response.profileObj);
+  }
+  render(){
+    return (
+      <Form className="login-form">
+        <h2 className="text-center">Welcome to Universal RX</h2>
+        <h6 className="text-center welcomedes">Universal Orthodontic Lab Digital Prescription</h6>
+        <FormGroup>
+          <Input className="acctlogin" type="email" placeholder="Username"></Input>
+        </FormGroup>
+        <FormGroup>
+          <Input className="acctlogin" type="password" placeholder="Password"></Input>
+        </FormGroup>
+        <div className="forgot-password text-left">
+          <a href="/forgot-password"><span className="pt-3">Forgot Your Password?</span></a>
+        </div>
+        <Button className="btn-lg btn-primary btn-block">Log in</Button>
+        <div className="text-center pt-3">Or continue with your social account</div>
+        <div class="row socialLogin">
+          <div class="col-md-6">
+            <GoogleLogin
+              clientId="448097078035-h3ikudenh75bdpriivj7bg8067dlcutr.apps.googleusercontent.com" 
+              className="mt-3 mb-3"
+              buttonText="Login with Google"
+              onSuccess={this.responseGoogle}
+              onFailure={this.responseGoogle}
+              cookiePolicy={'single_host_origin'}
+              theme= "dark" />
+          </div> 
+          <div class="col-md-6">
+            <MicrosoftLogin 
+            clientId="d0816670-4927-4395-a61e-47e964584112"
+            className="mt-3 mb-3"
+            buttonText="Login with Microsoft"
+            theme= "dark"
+            />
+          </div>
+        </div>
+        <div className="text-left pt-3">Do you have an account? 
+          
+          <Link to= '/SignUp' className="links"> Join free today</Link>
+        </div>
+      </Form>
+      
+    );
+  }
 }
 
 export default Login;
