ef589d19bec251c2bade3fafb750fa0d05e804d3
lenayj <lena@uniortholab.com>
2021-03-04 09:17:29 -0800

    commit on 'master'

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,10 +9,17 @@ import MicrosoftLogin from "react-microsoft-login";
 
 class Login extends Component {  
   responseGoogle=(response)=>{
-    console.log(response);
+    //console.log(response);
     console.log(response.profileObj);
+    console.log(response.profileObj.name);
+    console.log(response.profileObj.email);
   }
-  render(){
+  
+  
+
+ render(){
+  
+
     return (
       <Form className="login-form">
         <h2 className="text-center">Welcome to Universal RX</h2>
@@ -26,14 +33,13 @@ class Login extends Component {
         <div className="forgot-password text-left">
           <a href="/forgot-password"><span className="pt-3">Forgot Your Password?</span></a>
         </div>
-        <Button className="btn-lg btn-primary btn-block">Log in</Button>
+        <Button className="btn-lg btn-primary btn-block" type="submit">Log in</Button>
         <div className="text-center pt-3">Or continue with your social account</div>
         <div class="row socialLogin">
           <div class="col-md-6">
             <GoogleLogin
               clientId="448097078035-h3ikudenh75bdpriivj7bg8067dlcutr.apps.googleusercontent.com" 
               className="mt-3 mb-3"
-              buttonText="Login with Google"
               onSuccess={this.responseGoogle}
               onFailure={this.responseGoogle}
               cookiePolicy={'single_host_origin'}
@@ -41,10 +47,7 @@ class Login extends Component {
           </div> 
           <div class="col-md-6">
             <MicrosoftLogin 
-            clientId="d0816670-4927-4395-a61e-47e964584112"
-            className="mt-3 mb-3"
-            buttonText="Login with Microsoft"
-            theme= "dark"
+            
             />
           </div>
         </div>
