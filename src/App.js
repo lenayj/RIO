1eaedba4627c84c0c4a6a79f16b210ba4aea4a40
lenayj <lena@uniortholab.com>
2021-03-04 13:36:26 -0800

    Remove Header/Login Page MOD

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React,{Component} from 'react';
 import Routers from './components/Router'; 
 
-
 class App extends Component {
   render() {
       return (
         <>
-        <Routers/>
+          <Routers/>
         </>
-
       );
   }
 }
