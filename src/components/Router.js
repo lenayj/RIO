e1a6654e9ddda03c527574f960bd186b1a429c12
lenayj <lena@uniortholab.com>
2021-03-10 12:01:38 -0800

    forgot password

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './Login';
 import Signup from './Signup';
 import resetPassword from './resetPassword'
+import forgotPassword from './forgotPassword'
 
 export default () => (
     <Router>
         <Route path='/Login' component={Login}/>
         <Route path='/Signup' component={Signup}/>
         <Route path='/resetPassword' component={resetPassword}/>
+        <Route path='/forgotPassword' component={forgotPassword}/>
     </Router>
 )
