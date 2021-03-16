109683830b66f7baae2018e0ca4d52839160ac2c
lenayj <lena@uniortholab.com>
2021-03-16 14:07:04 -0700

    Add myCase page components

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './Header';
 import Login from './Login';
 import Signup from './Signup';
-import resetPassword from './resetPassword'
-import forgotPassword from './forgotPassword'
+import resetPassword from './resetPassword';
+import forgotPassword from './forgotPassword';
+import myCases from './myCases';
 
 export default () => (
     <Router>
+        <Header />
         <Route path='/Login' component={Login}/>
         <Route path='/Signup' component={Signup}/>
         <Route path='/resetPassword' component={resetPassword}/>
         <Route path='/forgotPassword' component={forgotPassword}/>
+        <Route path='/myCases' component={myCases}/>
     </Router>
 )
