8039c72b10321f2b675b9e5eb52fbe58adc070de
lenayj <lena@uniortholab.com>
2021-03-18 13:04:10 -0700

    Add pagination

--- src/components/myCases.js
+++ src/components/myCases.js
@@ -1,130 +1,160 @@
-import React, { Component } from 'react'
-
-class MyCases extends Component {
-  
-  constructor(props) {
-    super(props)
-    this.state = {
-      myCase: [],
-      caseId:'',
-      isLoading: false,
-      isError: false
-    }
-  }
-
-  /*async function get request*/
-  async componentDidMount() {
+import React, { PureComponent } from 'react'
+import axios from 'axios';
+import ReactPaginate from 'react-paginate';
+import '../css/pagination.css';
+import '../css/myCases.css';
     
-    this.setState({isLoading: true})
-    /*Get User Cases List data from below
-    :: URL mod needed here*/
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+export class MyCases extends PureComponent {
+
+    constructor(props) {
+        super(props)
     
-    if (response.ok) {
-      const myCase = await response.json()
-      //console.log(myCase)
-      this.setState({myCase,isLoading:false})
-    } else {
-      this.setState({isError:true,isLoading:false})
+        this.state = {
+            offset: 0,
+            tableData: [],
+            orgtableData: [],
+            perPage: 10,
+            currentPage: 0,
+            isLoading: false,
+            isError: false
+        }
+
+        this.handlePageClick = this.handlePageClick.bind(this);
+
     }
 
-  }
+    handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        const offset = selectedPage * this.state.perPage;
 
-/* Call Open Msg Modal */
-  openMSG = (e) => {
-    const case_id = e.target.id
-    const url = 'http://localhost:3000/renderOpenMSG'
-    window.open(url+'?msgurl="' + url + '"&caseid='+ case_id, "Messenger", "width=350, height=400")
-  }
+        this.setState({
+            currentPage: selectedPage,
+            offset: offset
+        }, () => {
+            this.loadMoreData()
+        });
 
- 
-  /* Set Attribute as a Table Header 
-  renderTableHeader = () => {
-    return Object.keys(this.state.myCase[0]).map(attr => <th key={attr}>
-      {attr.toUpperCase()}
-    </th>)
-  }*/
-  renderTableHeader = () => {
-    return <tr>
-    <th>CASE NO.</th>
-    <th>DATA SENT</th>
-    <th>PATIENT NAME</th>
-    <th>LABSLIP</th>
-    <th>STL FILES</th>
-    <th>STATUS</th>
-    <th>QUESTION</th>
-    </tr>
-  }
+    };
 
-  /* Set Data in a Table Row
-  :: Modify needed */
-  renderTableRows = () => {
-    return this.state.myCase.map(cases => {
-      return (
-        <tr key={cases.id}>
-          {/*Case No field 'case_id'*/}
-          <td>{cases.id}</td>
-          {/*Data Sent field 'reg_data'*/}
-          <td>{cases.name}</td>
-          {/*Patient Name field 'patient_first_name' + 'patient_last_name'*/}
-          <td>{cases.username}</td>
-          {/*Labslip field 'img_name'
-          :: Link required
-          :: <td><a href={cases.img_name} target="_blank" rel="noreferrer">View</a></td>*/}
-          <td>{cases.email}</td>
-          {/*STL File field 'file_name'
-          :: if !files -> Not Uploaded else File Link required*/}
-          <td>
-            {`${cases.address.street},${cases.address.city}`}
-          </td>
-          {/*Question field 'case_id'*/}
-          <td>{cases.website}</td>
-          {/*Message field 'case_id'*/}
-          <td><button id={cases.id} onClick={this.openMSG}>Leave a message</button></td>
-        </tr>
-      )
-    })
-  }
+    /* Call Open Msg Modal */
+    openMSG = (e) => {
+        const case_id = e.target.id
+        const url = 'http://localhost:3000/renderOpenMSG'
+        window.open(url+'?msgurl="' + url + '"&caseid='+ case_id, "Messenger", "width=350, height=400")
+    }
+
+    loadMoreData() {
+		const data = this.state.orgtableData;
+		
+		const slice = data.slice(this.state.offset, this.state.offset + this.state.perPage)
+		this.setState({
+			pageCount: Math.ceil(data.length / this.state.perPage),
+			tableData:slice
+		})
+	
+    }
+
+   componentDidMount(){
+        this.getData();
+    }
+    getData() {
+        axios
+            .get('https://jsonplaceholder.typicode.com/users')
+            .then(res => {
+                var tdata = res.data;
+                //console.log('data-->'+JSON.stringify(tdata))
+				var slice = tdata.slice(this.state.offset, this.state.offset + this.state.perPage)
+                this.setState({
+                    pageCount: Math.ceil(tdata.length / this.state.perPage),
+                    orgtableData : tdata,
+                    tableData:slice
+                })
+            });
+    }
 
 
-
-  render() {
-    const {myCase, isLoading, isError} = this.state
+    render() {
+        const {tableData,isLoading, isError} = this.state
     
-    if(isLoading){
-      return <div>Loading...</div>
-    }
+        if(isLoading){
+        return <div>Loading...</div>
+        }
 
-    if(isError){
-      return <div>Error...</div>
-    }
+        if(isError){
+        return <div>Error...</div>
+        }
+        return tableData.length > 0 
+        ? (
+            <div>
+                 <h1>My Cases</h1>
 
-    return myCase.length > 0
-    /* myCase.length is grater than 0 Loop ?(true) :(false) */
-    ? (
-      <div>
-        <div>
-          <h1 className="title-wording">My Cases</h1>
-        </div>
-        <div className="table-responsive">
-          <table className="table table-striped">
-            <thead>              
-                {this.renderTableHeader()}              
-            </thead>
-            <tbody id="user-cases">
-              {this.renderTableRows()}
-            </tbody>
-          </table>
-        </div>
-        <div className="case-pagination">
-	        <span className="left" id="total_reg"></span>
-	        <ul className="pagination pager" id="case-pager"></ul>
-	      </div>
-      </div>
-    ):(
-      <div>There is No Case List</div>
-    )
-  }
+                 <table border="1">
+                     <thead>
+                        <tr>
+                            <th>CASE NO.</th>
+                            <th>DATA SENT</th>
+                            <th>PATIENT NAME</th>
+                            <th>LABSLIP</th>
+                            <th>STL FILES</th>
+                            <th>STATUS</th>
+                            <th>QUESTION</th>
+                        </tr>
+                     </thead>
+                     {/*Todo:: Key modify required here*/}
+                     <tbody>
+                        {
+                          this.state.tableData.map((tdata, i) => (
+                                <tr key={tdata.id}>
+                                    {/*Case No field 'case_id'*/}
+                                    <td>{tdata.id}</td>
+                                    {/*Data Sent field 'reg_data'*/}
+                                    <td>{tdata.name}</td>
+                                    {/*Patient Name field 'patient_first_name' + 'patient_last_name'*/}
+                                    <td>{tdata.username}</td>
+                                    {/*Labslip field 'img_name'
+                                    :: Link required
+                                    :: <td><a href={tdata.img_name} target="_blank" rel="noreferrer">View</a></td>*/}
+                                    <td>{tdata.email}</td>
+                                    {/*STL File field 'file_name'
+                                    :: if !files -> Not Uploaded else File Link required
+                                    Refer)) <td>{`${tdata.website}` === 'kale.biz' 
+                                    ? <a href={tdata.img_name} className="stl_file" id='stl_file' target="_blank">View & Download</a> 
+                                    : 'Not Uploaded'}</td>*/}
+                                    <td>
+                                        {`${tdata.address.street},${tdata.address.city}`}
+                                    </td>
+                                    {/*STATUS field 'status'*/}
+                                    <td>{tdata.website}</td>
+                                    {/*Message field 'case_id'*/}
+                                    <td><button id={tdata.id} onClick={this.openMSG}>Leave a message</button></td>
+                                </tr>
+                            
+                          ))
+                        }
+
+                     </tbody>
+                 </table>   
+                
+                <div>{tableData.length} Entries in Total </div>
+
+                 <ReactPaginate
+                    previousLabel={"prev"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
+                    breakClassName={"break-me"}
+                    pageCount={this.state.pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={this.handlePageClick}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"active"}/>
+            </div>
+        ):(
+            <div>There is No Case List</div>
+          )
+    }
 }
 
 export default MyCases;
--- src/components/renderOpenMSG.js
+++ src/components/renderOpenMSG.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import '../css/messanger.css';
 
 class renderOpenMSG extends Component {
 
