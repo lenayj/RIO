c31da7fedad21c39e2b6f4c97b4a551a47d2bb58
lenayj <lena@uniortholab.com>
2021-03-24 14:53:57 -0700

    Dashboard css,js mod

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,70 @@
 import '../css/Header.css';
-import logo from '../assets/logo.png';
+import '../App.css';
 import {React, Component} from "react";
 
 import { Link } from 'react-router-dom';
+import { BiSearch } from "react-icons/bi";
+import { VscBell, VscGear, VscAccount } from "react-icons/vsc";
+
+/*Icon :: https://react-icons.github.io/react-icons/search?q=setting */
 
 class Header extends Component{
+   
+    /* Todo:: Search Button Click Event */
+    SearchBtnClick(){
+        
+    }
+
+    SettingBtnClick(){
+
+    }
+
+    /* Todo:: UserAccount click event */
+    UserAccountBtnClick() {
+        
+    }
 
     render(){
-        if(window.location.pathname === '/Login' || window.location.pathname === '/Signup' ||
+        if(window.location.pathname === '/Login' || 
+        window.location.pathname === '/signup' || window.location.pathname === '/privacyStatement' ||
         window.location.pathname === '/forgotPassword' || window.location.pathname === '/resetPassword' ||
         window.location.pathname === '/renderOpenMSG') return null;
         return (
             <header className="Header">
-                <Link to= '/App' className="links"><img src={logo} className="Logo" alt="logo" /></Link>
-                <nav className="Nav">
+                <div className="Navbar">
+                    <div className="leftSide">
+                        <div className="inner text-white pl-4">
+                            <Link to= '/' className="links"><h2>UNIVERSAL RX</h2></Link>
+                        </div>
+                    </div>
+                    <div className="rightSide">
+                        <div className="inner">
+                            <div className="navbar searchbar">
+                                    <input type="text" placeholder="Search by Patient"/><button type="submit" onClick={this.SearchBtnClick}><BiSearch /></button>
+                                </div>
+                            <div className="navbar alarm">
+                                <div><VscBell color="white" size="24"/></div>
+                            </div>
+                            <div className="navbar setting">
+                                <div onClick={this.SettingBtnClick}><VscGear color="white" size="24"/></div>
+                            </div>
+                            <div className="navbar UserDropdown">
+                                <div onClick={this.UserAccountBtnClick}><VscAccount color="white" size="24"/></div>
+                            </div>
+                        </div>
+                    </div>  
+                </div>
+                
+                <nav className="Navbar2">
                     <Link to= '/myCases' className="links">My Cases</Link>
                     <Link to= '/newCase' className="links">New Case</Link>
+                    <Link to= '/' className="links">Booking</Link>
+                    <Link to= '/myAccount' className="links">My Account</Link>
+                    <Link to= '/' className="links">Support</Link>
+                    <Link to= '/' className="links">View Invoices</Link>
+                    <a href="https://uniortholab.com/us/" target="_blank" rel="noreferrer" className="links">Go To UniversalLab.com</a>
+                    <Link to= '/' className="links">Need More Help?</Link>
+
                     {/*<Link to= '/SignUp' className="links">SignUp</Link>*/}
                 </nav>
             </header>    
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,34 @@
-import Header from "./Header"
+import '../css/Home.css';
+
 import React from 'react';
-import Header from '../components/Header';
+import { Link } from 'react-router-dom';
 
-
-function Main(){
+function Home(){
     return(
-        <>
-            <Header />
-            <div>Main</div>;
-        </>
+        <div className="homebg">
+            <div className="container home">
+                <div className="grettingMsg pt-4">
+                    {/* Todo :: Implement Login UserName */}
+                    <div className="greetingUsers pt-3"><h1>Welcome,</h1>{/*UserName is required*/}</div>
+                    <div className="text-secondary pt-2">Upload your Case and Book your pick-up online to avoid delay! <a href="http://uniortholab.com/us">Learn more</a></div>
+                </div>
+                <div className="menuIcon">
+                    <div className="row justify-content-md-center">
+                        <div className="col"><Link to= '/newcase' className="links"><img src="/images/main/m_newcase.png" alt="m_newcase"/></Link></div>
+                        <div className="col"><Link to= '/mycases' className="links"><img src="/images/main/m_mycases.png" alt="m_mycases"/></Link></div>
+                    </div>
+                    <div className="row">
+                        <div className="col"><Link to= '/' className="links"><img src="/images/main/m_booking.png" alt="m_booking"/></Link></div>
+                        <div className="col"><Link to= '/myAccount' className="links"><img src="/images/main/m_myinfo.png" alt="m_myinfo"/></Link></div>
+                    </div>
+                    <div className="row">
+                        <div className="col"><Link to= '/' className="links"><img src="/images/main/m_support.png" alt="m_support"/></Link></div>
+                        <div className="col"><Link to= '/' className="links"><img src="/images/main/m_viewinvoices.png" alt="m_viewinvoices"/></Link></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
-export default Main;
+export default Home;
