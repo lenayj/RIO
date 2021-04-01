7fb82b8fe8f8aa3cfdcf3b2a2900c32030f9fa70
lenayj <lena@uniortholab.com>
2021-04-01 12:56:11 -0700

    remove unused line from router

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Header from './Header';
-
-import Home from './Home';
 
 import Login from './Login';
 import Signup from './Signup';
@@ -14,8 +11,6 @@ import forgotPassword from './forgotPassword';
 
 export default () => (
     <Router>
-        <Header />
-        <Route path="/" exact component={Home} />
         <Route path='/Login' component={Login}/>
         <Route path='/signup' component={Signup}/>
         <Route path='/privacyStatement' component={privacyStatement}/>
