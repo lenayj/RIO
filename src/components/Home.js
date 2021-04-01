fce75e5bd128417db30ab1451d277f29af01e5b5
lenayj <lena@uniortholab.com>
2021-04-01 12:36:26 -0700

    Delete which not related to sign up and login

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,12 +14,12 @@ function Home(){
                 </div>
                 <div className="menuIcon">
                     <div className="row justify-content-md-center">
-                        <div className="col"><Link to= '/newcase' className="links"><img src="/images/main/m_newcase.png" alt="m_newcase"/></Link></div>
-                        <div className="col"><Link to= '/mycases' className="links"><img src="/images/main/m_mycases.png" alt="m_mycases"/></Link></div>
+                        <div className="col"><Link to= '/' className="links"><img src="/images/main/m_newcase.png" alt="m_newcase"/></Link></div>
+                        <div className="col"><Link to= '/' className="links"><img src="/images/main/m_mycases.png" alt="m_mycases"/></Link></div>
                     </div>
                     <div className="row">
                         <div className="col"><Link to= '/' className="links"><img src="/images/main/m_booking.png" alt="m_booking"/></Link></div>
-                        <div className="col"><Link to= '/myAccount' className="links"><img src="/images/main/m_myinfo.png" alt="m_myinfo"/></Link></div>
+                        <div className="col"><Link to= '/' className="links"><img src="/images/main/m_myinfo.png" alt="m_myinfo"/></Link></div>
                     </div>
                     <div className="row">
                         <div className="col"><Link to= '/' className="links"><img src="/images/main/m_support.png" alt="m_support"/></Link></div>
--- src/components/Router.js
+++ src/components/Router.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './Header';
 
 import Home from './Home';
-import myAccount from './myAccount';
 
 import Login from './Login';
 import Signup from './Signup';
@@ -12,29 +11,17 @@ import termsAndService from './termsAndService';
 import resetPassword from './resetPassword';
 import forgotPassword from './forgotPassword';
 
-import myCases from './myCases';
-import renderOpenMSG from './renderOpenMSG';
-
-import newCase from './newCase';
-import newCasePersonalInfo from './newCasePersonalInfo';
-import newCasePrescription from './newCasePrescription';
 
 export default () => (
     <Router>
         <Header />
         <Route path="/" exact component={Home} />
-        <Route path='/myAccount' component={myAccount}/>
         <Route path='/Login' component={Login}/>
         <Route path='/signup' component={Signup}/>
         <Route path='/privacyStatement' component={privacyStatement}/>
         <Route path='/termsAndService' component={termsAndService}/>
         <Route path='/resetPassword' component={resetPassword}/>
         <Route path='/forgotPassword' component={forgotPassword}/>
-        <Route path='/myCases' component={myCases}/>
-        <Route path='/renderOpenMSG' component={renderOpenMSG}/>
-        <Route path='/newCase' component={newCase}/>
-        <Route path='/newCasePersonalInfo' component={newCasePersonalInfo}/>
-        <Route path='/newCasePrescription' component={newCasePrescription}/>
 
     </Router>
 )
