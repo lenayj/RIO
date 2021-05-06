e1ef9f8b6e6bfbe6d63630442531f1f067f14d43
lenayj <lena@uniortholab.com>
2021-05-06 08:48:18 -0700

    Doctor/Office Information frontend modification

--- src/components/newCaseOfficeInfo.js
+++ src/components/newCaseOfficeInfo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 class NewCaseOfficeInfo extends Component {
 
@@ -7,14 +8,15 @@ class NewCaseOfficeInfo extends Component {
             <div className="doctorOfficeInfo mt-5">
                 <div className="part-header">
                     <h3>Doctor/Office Information</h3>
+                    <div className="address-edit"><Link to= '/myCases' target="_blank" className="links">Edit</Link></div>
                 </div>
+                
                 <div className="part-value">
-                    <div className="form-row">
-                        <div className="user-name float-left"><span>John Smith</span></div>
-                        <div className="defaultAddr-mark float-right"><p>&#10003; Your Default</p></div>
-                    </div>
-                    <div className="form-row doctor-license">Doctor License #:&nbsp; <span>12345</span></div>
-                    <div className="form-row user-address">Address:&nbsp; <span>11917 FRONT ST, NORWALK, CA 90650</span></div>
+                    <div className="defaultAddr-mark"><p>&#10003; Your Default</p></div>
+                    <div className="form-row user-name"><span>Doctor Name:&nbsp;</span> John Smith</div>
+                    <div className="form-row office-nema"><span>Office Name:&nbsp;</span> John Dentistry</div>
+                    <div className="form-row user-address"><span>Address:&nbsp;</span> 11917 FRONT ST, NORWALK, CA 90650</div>
+                    <div className="form-row phone"><span>Phone:&nbsp;</span> 000-000-0000</div>
                 </div>
             </div>
         );
