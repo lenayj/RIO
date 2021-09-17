c8ee849b3ce8a26432c19709783b3d2217b98e92
Venkatesh Revanur <venkatesh@uniortholab.com>
2021-09-17 07:53:18 -0700

    connect to Numen Server in staging

--- src/_services/axiosConfig.js
+++ src/_services/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:8080";
+const baseURL = process.env.REACT_APP_BASE_URL || "https://sapi.uniortholab.com";
 
 const instance = axios.create({
   // .. congigure axios baseURL
